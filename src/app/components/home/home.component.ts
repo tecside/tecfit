88573ae2129f4d8d05f87e43a12233068f638ad2
David <dmsm2000@hotmail.com>
2021-07-08 12:49:30 +0100

    Secção dos planos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import SwiperCore, { Autoplay } from 'swiper/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('swiperWrapper') public swiperWrapper: any;
+
+  config: any = {
+    a11y: false,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slideToClickedSlide: true,
+    mousewheel: true,
+    scrollbar: true,
+    watchSlidesProgress: true,
+    navigation: false,
+    keyboard: true,
+    pagination: { clickable: true },
+    centeredSlides: true,
+    loop: true,
+    roundLengths: false,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    spaceBetween: 0,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1
+      }
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSwiper(swiper: any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
+
+  slideNext() {
+    this.swiperWrapper.swiperRef.slideNext()
+  }
+
+  slidePrev() {
+    this.swiperWrapper.swiperRef.slidePrev()
   }
 
 }
