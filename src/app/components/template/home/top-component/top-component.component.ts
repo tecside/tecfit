15df507e09705d978ef73b4da9a0a974d6ff391b
David <dmsm2000@hotmail.com>
2021-07-22 01:11:59 +0100

    fade-in

--- src/app/components/template/home/top-component/top-component.component.ts
+++ src/app/components/template/home/top-component/top-component.component.ts
@@ -22,74 +22,97 @@ export class TopComponentComponent implements OnInit {
     this.headerImage2 = "../../../assets/home/header-image-02.jpg"
     this.headerImage3 = "../../../assets/home/header-image-03.jpg"
     this.headerImage4 = "../../../assets/home/header-image-04.jpg"
-   }
+  }
 
   ngOnInit(): void {
-    this.changeAlone = setInterval(()=>{
+    this.changeAlone = setInterval(() => {
       this.next();
-    },10000)
+    }, 10000)
   }
 
-  next(){
-    
-    this.page +=1;
-    if(this.pageNameArray.length <= this.page){
+  next() {
+
+    this.page += 1;
+    if (this.pageNameArray.length <= this.page) {
       this.page = 0
       this.pageName = this.pageNameArray[this.page]
-     
-    }else{ 
+
+    } else {
       this.pageName = this.pageNameArray[this.page]
     }
 
-    this.headerImage1 = "../../../assets/home/header-image-" + this.page +"1.jpg"
-    setTimeout(() => {   
-      this.headerImage2 = "../../../assets/home/header-image-" + this.page +"2.jpg"
+    this.headerImage1 = "../../../assets/home/header-image-" + this.page + "1.jpg"
+    var element = document.getElementsByClassName("header-image")[0];
+    element.classList.remove("fade-in");
+    setTimeout(() => {
+      element.classList.add("fade-in");
+    }, 1)
+
+    setTimeout(() => {
+      this.headerImage2 = "../../../assets/home/header-image-" + this.page + "2.jpg"
+      var element = document.getElementsByClassName("header-image")[1];
+      element.classList.remove("fade-in");
+      setTimeout(() => {
+        element.classList.add("fade-in");
+      }, 1)
+
     }, 50);
-    
-    setTimeout(() => {   
-      this.headerImage3 = "../../../assets/home/header-image-" + this.page +"3.jpg"
+
+    setTimeout(() => {
+      this.headerImage3 = "../../../assets/home/header-image-" + this.page + "3.jpg"
+      var element = document.getElementsByClassName("header-image")[2];
+      element.classList.remove("fade-in");
+      setTimeout(() => {
+        element.classList.add("fade-in");
+      }, 1)
+
     }, 100);
 
-    setTimeout(() => {   
-      this.headerImage4 = "../../../assets/home/header-image-" + this.page +"4.jpg"
+    setTimeout(() => {
+      this.headerImage4 = "../../../assets/home/header-image-" + this.page + "4.jpg"
+      var element = document.getElementsByClassName("header-image")[3];
+      element.classList.remove("fade-in");
+      setTimeout(() => {
+        element.classList.add("fade-in");
+      }, 1)
     }, 150);
 
     clearInterval(this.changeAlone);
-  
-    this.changeAlone = setInterval(()=>{
+
+    this.changeAlone = setInterval(() => {
       this.next();
-    },10000)
+    }, 10000)
   }
 
-  previus(){
-    this.page -=1;
-    
-    if(0 > this.page){
-      this.page = this.pageNameArray.length -1
+  previus() {
+    this.page -= 1;
+
+    if (0 > this.page) {
+      this.page = this.pageNameArray.length - 1
       this.pageName = this.pageNameArray[this.page]
-    }else{ 
+    } else {
       this.pageName = this.pageNameArray[this.page]
     }
 
-    this.headerImage4 = "../../../assets/home/header-image-" + this.page +"4.jpg"
+    this.headerImage4 = "../../../assets/home/header-image-" + this.page + "4.jpg"
 
-    setTimeout(() => {   
-      this.headerImage3 = "../../../assets/home/header-image-" + this.page +"3.jpg"
+    setTimeout(() => {
+      this.headerImage3 = "../../../assets/home/header-image-" + this.page + "3.jpg"
     }, 50);
 
-    setTimeout(() => {   
-      this.headerImage2 = "../../../assets/home/header-image-" + this.page +"2.jpg"
+    setTimeout(() => {
+      this.headerImage2 = "../../../assets/home/header-image-" + this.page + "2.jpg"
     }, 100);
 
-    setTimeout(() => {   
-      this.headerImage1 = "../../../assets/home/header-image-" + this.page +"1.jpg"
+    setTimeout(() => {
+      this.headerImage1 = "../../../assets/home/header-image-" + this.page + "1.jpg"
     }, 150);
 
     clearInterval(this.changeAlone);
 
-    this.changeAlone = setInterval(()=>{
+    this.changeAlone = setInterval(() => {
       this.next();
-    },10000)
+    }, 10000)
   }
 
 
