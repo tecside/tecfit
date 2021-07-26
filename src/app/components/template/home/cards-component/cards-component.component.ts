8469cc7f4fd4f6ef79d9adc7e83ddc6db1b433e9
David <dmsm2000@hotmail.com>
2021-07-26 19:53:56 +0100

    Inicio da versao mobile

--- src/app/components/template/home/cards-component/cards-component.component.ts
+++ src/app/components/template/home/cards-component/cards-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cards-component',
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponentComponent implements OnInit {
 
+  @ViewChild('swiperWrapper') public swiperWrapper: any;
+
+  config: any = {
+    a11y: false,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slideToClickedSlide: true,
+    mousewheel: true,
+    scrollbar: true,
+    watchSlidesProgress: true,
+    navigation: false,
+    keyboard: true,
+    pagination: { clickable: true },
+    centeredSlides: true,
+    loop: true,
+    roundLengths: false,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    spaceBetween: 0,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1
+      }
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSwiper(swiper: any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
+
+  slideNext() {
+    this.swiperWrapper.swiperRef.slideNext()
+  }
+
+  slidePrev() {
+    this.swiperWrapper.swiperRef.slidePrev()
+  }
+
+  
 
 }
--- src/app/components/template/home/recipes-component/recipes-component.component.ts
+++ src/app/components/template/home/recipes-component/recipes-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-component',
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponentComponent implements OnInit {
 
+  @ViewChild('swiperWrapper2') public swiperWrapper: any;
+
+  config: any = {
+    a11y: false,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slideToClickedSlide: true,
+    mousewheel: true,
+    scrollbar: true,
+    watchSlidesProgress: true,
+    navigation: false,
+    keyboard: true,
+    pagination: { clickable: true },
+    centeredSlides: true,
+    loop: true,
+    roundLengths: false,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    spaceBetween: 0,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1
+      }
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSwiper(swiper: any) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
+
+  slideNext() {
+    this.swiperWrapper.swiperRef.slideNext()
+  }
+
+  slidePrev() {
+    this.swiperWrapper.swiperRef.slidePrev()
+  }
+
 
 }
