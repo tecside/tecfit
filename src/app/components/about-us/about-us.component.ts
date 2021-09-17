fd68b5321e40aea69ba667b9ed2b83a7380bfc0e
David <dmsm2000@hotmail.com>
2021-09-17 21:05:27 +0100

    Alteração da timeline

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,6 +1,8 @@
 import { TimelineElement } from "./../horizontal-timeline/timeline-element";
 import { Component, OnInit } from "@angular/core";
 import SwiperCore, { Autoplay, EffectFade } from "swiper/core";
+import { TranslateServiceApp } from '../../services/translate.service'
+import { TranslateService } from "@ngx-translate/core";
 
 SwiperCore.use([Autoplay, EffectFade]);
 @Component({
@@ -10,25 +12,41 @@ SwiperCore.use([Autoplay, EffectFade]);
 })
 export class AboutUsComponent implements OnInit {
   timeline: TimelineElement[];
+  timeline2: TimelineElement[];
 
   constructor() {
-    
+
     this.timeline = [
-      { caption: '2019', date: new Date(2015, 1, 16),selected: true, title: 'Idealização', content: '' },
+      { caption: '2019', date: new Date(2015, 1, 16), selected: true, title: 'Idealização', content: '' },
       { caption: '2020', date: new Date(2016, 5, 16), title: 'Criação', content: '' },
       { caption: '2021', date: new Date(2017, 5, 16), title: 'Prmeiras', content: '' },
       { caption: '2021', date: new Date(2018, 5, 16), title: 'Expansão', content: '' },
       { caption: '2020', date: new Date(2019, 5, 16), title: 'Plataforma Fitness', content: '' },
-      ];
+    ];
+    this.timeline2 = [
+      { caption: '2019', date: new Date(2015, 1, 16), selected: true, title: 'Idealization', content: '' },
+      { caption: '2020', date: new Date(2016, 5, 16), title: 'Creation', content: '' },
+      { caption: '2021', date: new Date(2017, 5, 16), title: 'First', content: '' },
+      { caption: '2021', date: new Date(2018, 5, 16), title: 'Expansion', content: '' },
+      { caption: '2020', date: new Date(2019, 5, 16), title: 'Fitness Platform', content: '' },
+    ];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (localStorage.getItem('lang') == "pt") {
+      document.getElementById("timeline-pt")!.style.display = "block";
+      document.getElementById("timeline-en")!.style.display = "none";
+    } else {
+      document.getElementById("timeline-pt")!.style.display = "none";
+      document.getElementById("timeline-en")!.style.display = "block";
+    }
+  }
 
   onSwiper(swiper: any) {
-    console.log(swiper);
+    /* console.log(swiper); */
   }
   onSlideChange() {
-    console.log("slide change");
+    /* console.log("slide change"); */
   }
 
 }
--- src/app/services/translate.service.ts
+++ src/app/services/translate.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { ActivatedRoute, Router, RouterLinkActive } from "@angular/router";
 import { TranslateService, TranslateModule } from "@ngx-translate/core";
 
 @Injectable({
@@ -8,7 +9,7 @@ export class TranslateServiceApp {
 
   actualLang: string | null;
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private router: Router) {
     translate.addLangs(["pt", "en"]);
     translate.setDefaultLang("en");
     const browserLang = translate.getBrowserLang();
@@ -19,13 +20,23 @@ export class TranslateServiceApp {
     );
     localStorage.setItem("lang", translate.currentLang);
     this.actualLang = localStorage.getItem('lang')
-
   }
 
   changeLocale(locale: string) {
     this.translate.use(locale);
     localStorage.setItem("lang", locale);
     this.actualLang = locale;
-  }
 
+    if (String(this.router.url).includes("/about-us")) {
+      if (localStorage.getItem('lang') == "pt") {
+        document.getElementById("timeline-pt")!.style.display = "block";
+        document.getElementById("timeline-en")!.style.display = "none";
+      } else {
+        document.getElementById("timeline-pt")!.style.display = "none";
+        document.getElementById("timeline-en")!.style.display = "block";
+      }
+    }
+
+
+  }
 }
