a7b909b914ce29f217189912038dc663cd746e79
David <dmsm2000@hotmail.com>
2021-10-02 22:44:33 +0100

    slides

--- src/app/components/template/programs/programs.component.ts
+++ src/app/components/template/programs/programs.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { EffectCoverflow } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow]);
 
 @Component({
   selector: 'app-programs',
   templateUrl: './programs.component.html',
   styleUrls: ['./programs.component.scss']
 })
+
 export class ProgramsComponent implements OnInit {
 
   constructor() { }
