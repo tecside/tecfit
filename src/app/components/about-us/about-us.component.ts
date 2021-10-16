19bd5528f169fd2b9a41e9b312924385e8136105
David <dmsm2000@hotmail.com>
2021-10-16 23:12:07 +0100

    teste

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -10,37 +10,11 @@ SwiperCore.use([EffectCoverflow, Navigation, Autoplay]);
   styleUrls: ["./about-us.component.scss"],
 })
 export class AboutUsComponent implements OnInit {
- /*  timeline: TimelineElement[];
-  timeline2: TimelineElement[]; */
 
   constructor() {
-
-    /* this.timeline = [
-      { caption: '2019', date: new Date(2019, 12, 1), selected: true, title: 'A ideia nasce', content: 'A ideia nasce' },
-      { caption: '2020', date: new Date(2020, 6, 1), title: 'A ideia ganha um nome', content: 'A ideia ganha um nome' },
-      { caption: '2020', date: new Date(2021, 12, 1), title: 'A equipa foi criada', content: 'A equipa foi criada' },
-      { caption: '2021', date: new Date(2022, 6, 1), title: 'Primeira sessão ao vivo no Instagram', content: 'Primeira sessão ao vivo no Instagram' },
-      { caption: '2021', date: new Date(2023, 12, 1), title: 'Última sessão ao vivo no Instagram', content: 'Última sessão ao vivo no Instagram' },
-      { caption: '2021', date: new Date(2024, 12, 1), title: 'O grupo é reduzido para os actuais membros', content: 'O grupo é reduzido para os actuais membros' },
-    ];
-    this.timeline2 = [
-      { caption: '2019', date: new Date(2019, 12, 1), selected: true, title: 'The idea is born', content: 'The idea is born' },
-      { caption: '2020', date: new Date(2020, 6, 1), title: 'The idea gains a name', content: 'The idea gains a name' },
-      { caption: '2020', date: new Date(2021, 12, 1), title: 'The team is created', content: 'The team is created' },
-      { caption: '2021', date: new Date(2022, 6, 1), title: 'First live session on Instagram', content: 'First live session on Instagram' },
-      { caption: '2021', date: new Date(2023, 12, 1), title: 'Last live session on Instagram', content: 'Last live session on Instagram' },
-      { caption: '2021', date: new Date(2024, 12, 1), title: 'The group is reduced to the current members', content: 'The group is reduced to the current members' },
-    ]; */
   }
 
   ngOnInit(): void {
-    /* if (localStorage.getItem('lang') == "pt") {
-      document.getElementById("timeline-pt")!.style.display = "block";
-      document.getElementById("timeline-en")!.style.display = "none";
-    } else {
-      document.getElementById("timeline-pt")!.style.display = "none";
-      document.getElementById("timeline-en")!.style.display = "block";
-    } */
   }
 
   onSwiper(swiper: any) {
--- src/app/components/template/home/new-top/new-top.component.ts
+++ src/app/components/template/home/new-top/new-top.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Autoplay, EffectFade } from 'swiper';
+
+SwiperCore.use([EffectFade, Autoplay]);
 
 @Component({
   selector: 'app-new-top',
   templateUrl: './new-top.component.html',
   styleUrls: ['./new-top.component.scss']
 })
+
 export class NewTopComponent implements OnInit {
+
+  actual: number = 0;
+
+  paths: string[] = [
+    'assets/home/phones/phone1.png', 
+    'assets/home/phones/phone2.png', 
+    'assets/home/phones/phone3.png', 
+    'assets/home/phones/phone4.png'
+  ]
+
+  phone: string = this.paths[this.actual];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.changeAlone()
   }
 
+  changeAlone(): void {
+    setTimeout(() => {
+      this.actual++; 
+      if(this.actual == 4) {
+        this.actual = 0;
+      }
+      this.phone = this.paths[this.actual]      
+      this.changeAlone()
+    }, 3000)
+  }
 }
